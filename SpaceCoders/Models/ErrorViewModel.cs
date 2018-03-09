07f22efd6ee3249aead7539cff5362b8036b7dd3
Hunter Wright <wright.hunter@gmail.com>
2018-03-09 13:52:01 -0600

    Initial Project Addition

---
+++ SpaceCoders/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpaceCoders.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
